81a77b432f0d197c6e86a056916cf1a0bfe8f218
Blessen Joel Gorinkala <g.blessenjoel@gmail.com>
2026-02-06 00:04:16 +0530

    Update Helpful_maths.py

--- codeforces/Helpful_maths.py
+++ codeforces/Helpful_maths.py
@@ -1,2 +1,9 @@
-print("logic is being building")
-print("code is incoming")
+num = []
+fsum = []
+ran_som = input()
+num = ran_som.split("+")
+num.sort()
+fsum = num[0]
+for x in range(len(num)-1):
+    fsum = fsum+"+"+num[x+1]
+print(fsum)
