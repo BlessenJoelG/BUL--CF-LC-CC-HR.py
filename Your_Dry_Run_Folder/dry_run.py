9188ae471c406253d14f95f3a72956e2bbcf7d60
Blessen Joel Gorinkala <g.blessenjoel@gmail.com>
2026-02-23 11:42:17 +0530

    Replace code in dry_run.py with a pass statement
    Replace existing code with a placeholder 'pass' statement.

--- Your_Dry_Run_Folder/dry_run.py
+++ Your_Dry_Run_Folder/dry_run.py
@@ -1,13 +1,1 @@
-G_name,H_name,P_name,chk1,chk2= input(),input(),input(),[],[]
-for _ in range(0,len(G_name)):
-    chk1.append(G_name[_])
-for _ in range(0,len(H_name)):
-    chk1.append(H_name[_])
-chk1.sort()
-for _ in range(0,len(P_name)):
-    chk2.append(P_name[_])
-chk2.sort()
-if chk1 == chk2:
-    print("YES")
-else:
-    print("NO")
+pass
