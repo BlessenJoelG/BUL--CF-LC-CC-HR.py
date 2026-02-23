41df08645ea1f1710a7236881d09f26e358a18b4
Blessen Joel Gorinkala <g.blessenjoel@gmail.com>
2026-02-23 19:45:50 +0530

    Remove implementation of index operation

--- Your_Dry_Run_Folder/dry_run.py
+++ Your_Dry_Run_Folder/dry_run.py
@@ -1,9 +1,1 @@
-tokenizer,idx_op = [],[]
-for _ in range(len(words)):
-    for y in words[_]:
-        tokenizer.append(y)
-    if x in tokenizer:
-        idx_op.append(_)
-    tokenizer.clear()
-return idx_op
-# words = ["abc","bcd","aaaa","cbc"], x = "a"
+pass
