9b5e1ad95ab4e1a33417415714f0705302a60a2e
BlessenJoelG <154801802+BlessenJoelG@users.noreply.github.com>
2026-02-24 10:42:08 +0530

    1st  q of day

---
+++ CODEFORCES-PRAC/DIFF @800-900/1791A - Codeforces Checking.py
@@ -0,0 +1,8 @@
+check_list = ["c","o","d","e","f","r","s"]
+t = int(input())
+for _ in range(t):
+    x = input()
+    if x in check_list:
+        print("YES")
+    else:
+        print("NO")
--- Your_Dry_Run_Folder/dry_run.py
+++ Your_Dry_Run_Folder/dry_run.py
@@ -1,1 +1,9 @@
-pass
+t = int(input())
+for a in range(t):
+    alpha = str()
+    x = input()
+    x = list(x)
+    x[0],x[4] = x[4],x[0]
+    for _ in x:
+        alpha = alpha + _
+    print(alpha)
