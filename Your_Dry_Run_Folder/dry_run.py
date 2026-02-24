3bb18f4337696feb235088a577bcadf228b4fac1
Blessen Joel Gorinkala <g.blessenjoel@gmail.com>
2026-02-24 15:42:46 +0530

    Remove implementation and add pass statement

--- Your_Dry_Run_Folder/dry_run.py
+++ Your_Dry_Run_Folder/dry_run.py
@@ -1,6 +1,1 @@
-jewels,stones = input(),input()
-c = 0
-chk,pattern = [jewels[_]for _ in range(len(jewels))],[stones[x]for x in range(len(stones))]
-for _ in chk:
-    c = c + pattern.count(_)
-print(c)
+pass
