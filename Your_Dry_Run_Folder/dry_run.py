c16ab50351076e91f63cc0ee1bdf436a3566c48f
Blessen Joel Gorinkala <g.blessenjoel@gmail.com>
2026-02-24 10:44:08 +0530

    Update dry_run.py

--- Your_Dry_Run_Folder/dry_run.py
+++ Your_Dry_Run_Folder/dry_run.py
@@ -1,9 +1,1 @@
-t = int(input())
-for a in range(t):
-    alpha = str()
-    x = input()
-    x = list(x)
-    x[0],x[4] = x[4],x[0]
-    for _ in x:
-        alpha = alpha + _
-    print(alpha)
+pass
