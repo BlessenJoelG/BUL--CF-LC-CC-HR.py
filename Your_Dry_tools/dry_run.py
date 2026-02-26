cf51c629c05cd48c267ea7865774e094e6f89da1
BlessenJoelG <154801802+BlessenJoelG@users.noreply.github.com>
2026-02-26 12:13:12 +0530

    initial commit of the day

--- Your_Dry_tools/dry_run.py
+++ Your_Dry_tools/dry_run.py
@@ -1,7 +1,1 @@
-class Solution:
-    def reverseDegree(self, s: str) -> int:
-        alpha_rev_values,calc = dict(a=26,b=25,c=24,d=23,e=22,f=21,g=20,h=19,i=18,j=17,k=16,l=15,m=14,n=13,o=12,p=11,q=10,r=9,s=8,t=7,u=6,v=5,w=4,x=3,y=2,z=1),0
-        sstring_split = [s[idx] for idx in range(len(s))]
-        for _ in range(len(sstring_split)):
-            calc = calc + alpha_rev_values[sstring_split[_]]*(_+1)
-        return(calc)
+pass
