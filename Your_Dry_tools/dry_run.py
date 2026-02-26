c17cf749d032e9e4c512f3b4300ca195206b6146
Blessen Joel Gorinkala <g.blessenjoel@gmail.com>
2026-02-27 02:52:18 +0530

    Remove dry_run logic and replace with pass
    Removed the main logic and replaced it with a pass statement.

--- Your_Dry_tools/dry_run.py
+++ Your_Dry_tools/dry_run.py
@@ -1,12 +1,1 @@
-t = int(input())
-for idx in range(t):
-    x,c = input(),""
-    y = ["q" if x[_] == "p" else "p" if x[_] == "q" else "w" for _ in range(len(x))]
-    for i in y:
-        c = c + i
-    print(c[::-1])
-"""pwp
-qqqqq
-pppwwwqqq
-qpwwpqwpqpw
-pqpqpqpq"""    
+pass
